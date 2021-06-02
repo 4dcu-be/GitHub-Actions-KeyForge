b068986264117c9923e07533b35cec61b5ee82df
autoblack <sebastian.proost@gmail.com>
2021-06-02 07:23:15 +0000

    fixup: Format Python code with Black

--- scripts/get_keyforge.py
+++ scripts/get_keyforge.py
@@ -4,18 +4,18 @@ import json
 from datetime import datetime
 
 
-def get_keyforge(url='https://www.keyforgegame.com/api/decks/count/'):
+def get_keyforge(url="https://www.keyforgegame.com/api/decks/count/"):
     ssl_context = ssl._create_unverified_context()
-    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    req = Request(url, headers={"User-Agent": "Mozilla/5.0"})
     with urlopen(req, context=ssl_context) as response:
         html = response.read()
 
-    return json.loads(html.decode('utf-8'))
+    return json.loads(html.decode("utf-8"))
 
 
 if __name__ == "__main__":
 
     count_dict = get_keyforge()
 
-    with open("./data/keyforge_decks.tsv", mode='a+') as fout:
-        print(datetime.now(), count_dict["count"], sep='\t', file=fout)
+    with open("./data/keyforge_decks.tsv", mode="a+") as fout:
+        print(datetime.now(), count_dict["count"], sep="\t", file=fout)
